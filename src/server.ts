2ec7b1e82ddf4e43e8638f824fcd9e916edb5dbc
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-29 16:03:29 +0600

    server.ts

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+import app from './app'
+import config from './app/config'
+
+async function bootstrap() {
+  try {
+    await app.listen((await config.port) || 4000, () => {
+      console.log(`Port Listened: ${config.port || 4000}`)
+    })
+  } catch (error) {
+    console.log('Server Failed To Start')
+  }
+}
+
+bootstrap()
