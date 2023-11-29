0c34cc1acf82b8804a78cba90c4467f0c9258d33
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-29 20:49:47 +0600

    User Zod Validation

---
+++ src/app/modules/User/User.validation-zod.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+export const UserZodSchema = z
+  .object({
+    id: z
+      .number({
+        required_error: 'Unique ID Must Provided',
+        invalid_type_error: 'Only Number'
+      })
+      .positive({ message: 'Invalid ID' }),
+    name: z
+      .string()
+      .min(2, 'Name Too Much Short')
+      .max(50, 'Name Too Much Long'),
+    role: z.enum(['Admin', 'User', 'Normal']),
+    security: z.string().optional()
+  })
+  .strict()
